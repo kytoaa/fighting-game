3e1d9013f1009a7c2c953c284d2d12691eba15dd
kytoaa <58483576+kytoaa@users.noreply.github.com>
2025-09-17 08:30:30 +0100

    new fafnir sprites

--- fighting-game/src/characters/sol/specials/fafnir.rs
+++ fighting-game/src/characters/sol/specials/fafnir.rs
@@ -132,21 +132,25 @@ impl Player for Sol<Fafnir> {
     fn frame_name(&self) -> Option<(Box<str>, Vector2)> {
         const START_FRAME: usize = 4;
         const RECOVERY_FRAME: usize = FAFNIR_STARTUP + FAFNIR_ACTIVE + FAFNIR_RECOVERY / 2;
-        const FLAME_END_FRAME: usize = RECOVERY_FRAME - 4;
+        const FLAME_END_FRAME: usize = RECOVERY_FRAME - 5;
 
         Some(match self.frame {
             0..START_FRAME => ("sol/specials/fafnir/fafnir1".into(), BASE_SPRITE_OFFSET),
             START_FRAME..FAFNIR_STARTUP => (
                 "sol/specials/fafnir/fafnir2".into(),
-                BASE_SPRITE_OFFSET + Vector2::LEFT * 8.0 * self.dir(),
+                BASE_SPRITE_OFFSET + Vector2::LEFT * 10.0 * self.dir(),
             ),
-            FAFNIR_STARTUP..RECOVERY_FRAME => (
+            FAFNIR_STARTUP..FLAME_END_FRAME => (
                 "sol/specials/fafnir/fafnir3".into(),
-                BASE_SPRITE_OFFSET + Vector2::LEFT * 8.0 * self.dir(),
+                BASE_SPRITE_OFFSET + Vector2::LEFT * 22.0 * self.dir(),
+            ),
+            FLAME_END_FRAME..RECOVERY_FRAME => (
+                "sol/specials/fafnir/fafnir4".into(),
+                BASE_SPRITE_OFFSET + Vector2::LEFT * 10.0 * self.dir(),
             ),
             RECOVERY_FRAME.. => (
                 "sol/specials/fafnir/fafnir5".into(),
-                BASE_SPRITE_OFFSET + Vector2::LEFT * 8.0 * self.dir(),
+                BASE_SPRITE_OFFSET + Vector2::LEFT * 10.0 * self.dir(),
             ),
         })
     }
